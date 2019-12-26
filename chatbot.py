f6a715203168cc9021fcb949cd66679e13ca8e7e
Harshil <37377066+harshil21@users.noreply.github.com>
2019-12-27 00:59:54 +0400

    learns anything

--- chatbot.py
+++ chatbot.py
@@ -9,7 +9,7 @@ shanisirbot = chatterbot.ChatBot('The Shani Sir Bot',
                                                  'chatterbot.logic.MathematicalEvaluation',
                                                  'chatterbot.logic.UnitConversion',],
                                  preprocessors=['chatterbot.preprocessors.clean_whitespace'],
-                                 read_only=True)  # This disables further learning from conversations the bot has
+                                 read_only=False)  # This disables further learning from conversations the bot has
 
 def train_him():
     """Trains the bot using the standard English corpora (for now)"""
