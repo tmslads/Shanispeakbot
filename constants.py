2e90d1a0ebbd56f68b21d2aa1c93cd30087a7f45
Uncle Sam <samirmahmood002@gmail.com>
2020-03-13 14:46:15 +0400

    Updated links to Shanisirmodule, interactions

--- constants.py
+++ constants.py
@@ -5,5 +5,5 @@ group_ids = {'12b': '-1001396726510', 'grade12': '-1001210862980', 'wait': '-100
 # Websites to scrape from-
 
 _DOWNLOAD = "https://raw.githubusercontent.com"
-_LINK = "https://github.com/tmslads/Shanisirmodule/tree/haunya/Assets/clips"
+_LINK = "https://github.com/tmslads/Shanisirmodule/tree/master/Assets/clips"
 URL = 'http://randomfactgenerator.net/'  # To be scraped for facts()
