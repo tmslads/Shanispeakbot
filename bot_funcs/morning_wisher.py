6fe8115b75e4624160a8184231360ff2e2cd2ede
harshil21 <ilovebhagwan@gmail.com>
2020-05-07 18:11:03 +0400

    fixed bug where morning msg was sent after 11am

--- bot_funcs/morning_wisher.py
+++ bot_funcs/morning_wisher.py
@@ -21,7 +21,10 @@ def morning_goodness(context: CallbackContext) -> None:
     diff = right_now - context.bot_data['last_sent']
 
     # Send only if it has been over a day and is before 11am next morning since last good morning message-
-    if diff.days < 1 and right_now >= afternoon:
+    if diff.days < 1:
+        return
+
+    if right_now >= afternoon:
         return
 
     with open("files/good_mourning.txt", "r") as greetings:
